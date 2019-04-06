14b4ba54f4c95163e830876fde2341cf9ecd0f4a
Omar HABIBI <mrhabibiomar@gmail.com>
2019-04-06 04:37:48 -1100

    "Ajout du scénario de test de configuration"

---
+++ javaparser-core-testing/src/test/java/com/github/javaparser/SetupTest.java
@@ -0,0 +1,14 @@
+package com.github.javaparser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SetupTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
